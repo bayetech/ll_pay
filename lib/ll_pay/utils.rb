30aaa09fc0ec08fcabd557b6ea37b08604c08599
houdelin <houdelin@bayekeji.com>
2016-10-13 15:01:04 +0800

    add ll_pay hash utils

---
+++ lib/ll_pay/utils.rb
@@ -0,0 +1,11 @@
+module Llpay
+  module Utils
+    def self.stringify_keys(hash)
+      new_hash = {}
+      hash.each do |key, value|
+        new_hash[(key.to_sym rescue key) || key] = value
+      end
+      new_hash
+    end
+  end
+end
