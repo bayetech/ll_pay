7a375045329631a5130b1196a33d6d1efe11f87e
houdelin <houdelin@bayekeji.com>
2016-10-17 13:52:23 +0800

    fix service payment

--- lib/ll_pay/service.rb
+++ lib/ll_pay/service.rb
@@ -3,16 +3,31 @@ require 'json'
 module LlPay
   module Service
 
-    SECURITY_PAY_REQUIRED_PARAMS = %w(notify_url no_order dt_order busi_partner money_order valid_order user_id)
+    SECURITY_PAY_REQUIRED_PARAMS = %w(busi_partner dt_order info_order money_order name_goods no_order notify_url oid_partner risk_item sign_type valid_order)
     def self.securitypay_pay_json(params, options = {})
-      params = LlPay::Utils.stringify_keys(params)
-      options = LlPay::Utils.stringify_keys(options)
+      # params = LlPay::Utils.stringify_keys(params)
+      # options = LlPay::Utils.stringify_keys(options)
 
-      params[:sign_type] = options[:sign_type] || LlPay.sign_type
+      check_required_params(params, SECURITY_PAY_REQUIRED_PARAMS)
 
-      params = { oid_partner: LlPay.oid_partner }.merge(params)
+      params['sign_type'] = options['sign_type'] || LlPay.sign_type
 
-      params[:sign] = LlPay::Sign.generate(params)
+      params = { 'oid_partner' => LlPay.oid_partner }.merge(params)
+
+      sign_params = { 'busi_partner' => params['busi_partner'],
+                      'dt_order' => params['dt_order'],
+                      'info_order' => params['info_order'],
+                      'money_order' => params['money_order'],
+                      'name_goods' => params['name_goods'],
+                      'no_order' => params['no_order'],
+                      'notify_url' => params['notify_url'],
+                      'oid_partner' => params['oid_partner'],
+                      'risk_item' => params['risk_item'],
+                      'sign_type' => params['sign_type'],
+                      'valid_order' => params['valid_order']
+                    }
+
+      params['sign'] = LlPay::Sign.generate(sign_params)
 
       params.to_json
     end
