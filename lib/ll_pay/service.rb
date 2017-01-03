eb19726d127a8eefcae80f2b74ac713d447a75b4
houdelin <houdelin@bayekeji.com>
2017-01-03 15:07:40 +0800

    move check_required_params to utils

--- lib/ll_pay/service.rb
+++ lib/ll_pay/service.rb
@@ -5,7 +5,7 @@ module LlPay
 
     SECURITY_PAY_REQUIRED_PARAMS = %w(busi_partner dt_order info_order money_order name_goods no_order notify_url oid_partner risk_item sign_type valid_order)
     def securitypay_pay_json(params, options = {})
-      check_required_params(params, SECURITY_PAY_REQUIRED_PARAMS)
+      LlPay::Utils.check_required_params(params, SECURITY_PAY_REQUIRED_PARAMS)
 
       params['sign_type'] = params['sign_type'] || LlPay.sign_type
 
@@ -25,12 +25,6 @@ module LlPay
       params['sign'] = LlPay::Sign.generate(sign_params)
 
       params.to_json
-    end
-
-    def check_required_params(params, names)
-      names.each do |name|
-        warn("ll_pay Warn: missing required option: #{name}") unless params.has_key?(name)
-      end
     end
   end
 end
--- lib/ll_pay/utils.rb
+++ lib/ll_pay/utils.rb
@@ -7,5 +7,11 @@ module LlPay
       end
       new_hash
     end
+
+    def self.check_required_params(params, names)
+      names.each do |name|
+        warn("ll_pay Warn: missing required option: #{name}") unless params.has_key?(name)
+      end
+    end
   end
 end
