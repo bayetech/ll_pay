708ebf8b91574906ee6c9b213c9147ecfe1a9f0c
houdelin <houdelin@bayekeji.com>
2017-01-04 18:15:34 +0800

    recode service securitypay_pay_json and test

--- lib/ll_pay/service.rb
+++ lib/ll_pay/service.rb
@@ -6,23 +6,24 @@ module LlPay
     SECURITY_PAY_REQUIRED_PARAMS = %w(busi_partner dt_order info_order money_order name_goods no_order notify_url oid_partner risk_item sign_type valid_order)
     def securitypay_pay_json(params, options = {})
       LlPay::Utils.check_required_params(params, SECURITY_PAY_REQUIRED_PARAMS)
+      params = LlPay::Utils.stringify_keys(params)
+      options = LlPay::Utils.stringify_keys(options)
 
-      params['sign_type'] = params['sign_type'] || LlPay.sign_type
+      params[:sign_type] = params[:sign_type] || options[:sign_type] || LlPay.sign_type
+      params[:oid_partner] = params[:oid_partner] || options[:oid_partner] || LlPay.oid_partner
 
-      params = { 'oid_partner' => LlPay.oid_partner }.merge(params)
-
-      sign_params = { 'busi_partner' => params['busi_partner'],
-                      'dt_order' => params['dt_order'],
-                      'money_order' => params['money_order'],
-                      'no_order' => params['no_order'],
-                      'notify_url' => params['notify_url'],
-                      'oid_partner' => params['oid_partner'],
-                      'risk_item' => params['risk_item'],
-                      'sign_type' => params['sign_type'],
-                      'valid_order' => params['valid_order']
+      sign_params = { busi_partner: params[:busi_partner],
+                      dt_order: params[:dt_order],
+                      money_order: params[:money_order],
+                      no_order: params[:no_order],
+                      notify_url: params[:notify_url],
+                      oid_partner: params[:oid_partner],
+                      risk_item: params[:risk_item],
+                      sign_type: params[:sign_type],
+                      valid_order: params[:valid_order]
                     }
 
-      params['sign'] = LlPay::Sign.generate(sign_params)
+      params[:sign] = LlPay::Sign.generate(sign_params, options)
 
       params.to_json
     end
--- spec/ll_pay/service_spec.rb
+++ spec/ll_pay/service_spec.rb
@@ -4,11 +4,17 @@ describe LlPay::Service do
   describe '#test service' do
     specify 'test service securitypay_pay_json' do
       payment_params = {
+            ap_merchant_id: 'test',
             user_id: 1,
             no_order: 1,
             dt_order: '20161013153529',
+            oid_partner: LlPay.oid_partner,
             busi_partner: 109001,
             money_order: 100,
+            info_order: 'info_order',
+            name_goods: 'name_goods',
+            sign_type: 'RSA',
+            valid_order: '10080',
             risk_item: { user_info_mercht_userno: 1,
                          user_info_dt_register: '20161013153529',
                          frms_ware_category: 4008 }.to_json,
@@ -16,8 +22,9 @@ describe LlPay::Service do
           }
 
       r = LlPay.securitypay_pay_json(payment_params)
+      expect_result_json = "{\"ap_merchant_id\":\"test\",\"user_id\":1,\"no_order\":1,\"dt_order\":\"20161013153529\",\"oid_partner\":\"201103171000000000\",\"busi_partner\":109001,\"money_order\":100,\"info_order\":\"info_order\",\"name_goods\":\"name_goods\",\"sign_type\":\"RSA\",\"valid_order\":\"10080\",\"risk_item\":\"{\\\"user_info_mercht_userno\\\":1,\\\"user_info_dt_register\\\":\\\"20161013153529\\\",\\\"frms_ware_category\\\":4008}\",\"notify_url\":\"https://www.xxx.com/payments/1/notify\",\"sign\":\"qqcwimfOewsHFicywhywUKPkPI4ho0V2uf/4OSOINrxjt3Tfp+coX65g7AHYOxd9pnrmkb2FrzWqjBa96EwwOUUZPpgi3R5OCo7bSj1ljcJTesCkumKCLjDsmcZhmNz3zrnfVBpzNP0XsVB28IW4AwkMYO5maWAYIlsABMY4xsw=\"}"
 
-      expect(r).to eq("{\"oid_partner\":\"201103171000000000\",\"user_id\":1,\"no_order\":1,\"dt_order\":\"20161013153529\",\"busi_partner\":109001,\"money_order\":100,\"risk_item\":\"{\\\"user_info_mercht_userno\\\":1,\\\"user_info_dt_register\\\":\\\"20161013153529\\\",\\\"frms_ware_category\\\":4008}\",\"notify_url\":\"https://www.xxx.com/payments/1/notify\",\"sign_type\":\"MD5\",\"sign\":\"14b4dc8725b6c22326c2017319854190\"}")
+      expect(r).to eq(expect_result_json)
     end
   end
 end
